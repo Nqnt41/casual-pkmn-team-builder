313760d8dff7f681952698a85091b013a883776d
Nqnt41 <covenyr@ufl.edu>
2024-09-16 15:04:52 -0400

    Updated css with icon coloring per Pokemon type. Team setup created. Small other improvements.

--- src/App.js
+++ src/App.js
@@ -14,40 +14,6 @@ function App() {
         fetchAPI();
     }, []);
 
-    const addToTeam = async (key) => {
-        if (key != null) {
-            if (team.length < 6) {
-                setTeam([...team, fetchedData]);
-                console.log(team.length);
-
-                setNote("");
-            }
-            else {
-                setNote("Team is full!");
-            }
-        }
-        const fetchedData = await fetchPokemonData();
-
-        if (fetchedData == null) {
-            setNote("Not a valid ID!");
-        }
-        else if (team.length < 6) {
-            setTeam([...team, fetchedData]);
-            console.log(team.length);
-
-            setNote("");
-        }
-        else {
-            setNote("Team is full!");
-        }
-    };
-
-    const removeFromTeam = async (member) => {
-        const newTeam = team.filter((pokemon) => pokemon !== member);
-
-        setTeam(newTeam);
-    };
-
     const fetchAPI = async () => {
         try {
             Axios.get(`https://pokeapi.co/api/v2/pokemon/?limit=1025`).then((res) => {
@@ -59,15 +25,19 @@ function App() {
         }
     };
 
-    const fetchPokemonData = async () => {
+    const fetchPokemonData = async (member) => {
+        if (member == null) {
+            member = id;
+        }
+
         try {
-            if (!/^\d+$/.test(id) && !/^[a-zA-Z]+$/.test(id)) {
+            if (!/^\d+$/.test(member) && !/^[a-zA-Z]+$/.test(member)) {
                 setData("");
                 setID(null);
                 return null;
             }
             else {
-                const res = await Axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`);
+                const res = await Axios.get(`https://pokeapi.co/api/v2/pokemon/${member}`);
                 setData(res.data);
                 return res.data;
             }
@@ -78,49 +48,90 @@ function App() {
         }
     };
 
-    const printAllImages = () => {
+    const addToTeam = async (member) => {
+        const fetchedData = await fetchPokemonData(member);
+
+        if (fetchedData == null) {
+            setNote("Not a valid ID!");
+        } else if (team.length < 6) {
+            setTeam([...team, fetchedData]);
+            console.log(team.length);
+
+            setNote("");
+        } else {
+            setNote("Team is full!");
+        }
+    };
+
+    const removeFromTeam = async (member) => {
+        const newTeam = team.filter((pokemon) => pokemon !== member);
+
+        setTeam(newTeam);
+    };
+
+    const printTeamImages = () => {
         return (
             <div>
-                {api.map((pokemon, index) => (
+                {team.map((pokemon, index) => (
                     <img
+                        className={`teamSprite behindSprite ${pokemon.types[1] ? 'behindSprite2' : ''}`}
+                        data-type={pokemon.types[0].type.name}
+                        data-type2={pokemon.types[1] ? pokemon.types[1].type.name : ''}
                         key={index}
-                        src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index + 1}.png`}
+                        src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`}
                         alt={pokemon.name}
-                        onClick={() => addToTeam(key)}
+                        onClick={() => removeFromTeam(pokemon)}
                     />
                 ))}
             </div>
         );
     };
 
-    // TODO: Bigger goal next, maybe its own page. List buttons for every single pokemon, or at least 10. clicking adds to team and removes them from list.
-    // TODO: Maybe start with just listing 10 sprites. Then make them console.log buttons. Then have them add to team.
-    //printAllImages();
+    const printAllImages = () => {
+        return (
+            <div>
+                {api.map((pokemon, index) => (
+                    <img
+                        className="pokemonSprite"
+                        key={index}
+                        src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index + 1}.png`}
+                        alt={pokemon.name}
+                        onClick={() => addToTeam(index + 1)}
+                    />
+                ))}
+            </div>
+        );
+    };
 
+    // TODO: do images instead of text
+    // printAllImages();
 
     return (
-      <div className="App">
-        <input
-          placeholder="Pokemon ID"
-          onChange={(event) => {
-            setID(event.target.value);
-          }}
-          onKeyDown={(event) => {
-              if (event.key === 'Enter') {
-                  addToTeam();
-              }
-          }}
-        />
-        <button onClick={() => addToTeam()}> Print </button>
-        <h1> {data?.name} </h1>
-        {team.map((member, index) => (
-          <h2 onClick={() => removeFromTeam(member)} key={index}>{index + ". " + member?.name}</h2>
-        ))}
-        <h3> {'>' + note} </h3>
+        <div className="App">
 
-        {printAllImages()}
+            {printTeamImages()}
 
-      </div>
+            <input
+                placeholder="Pokemon ID"
+                onChange={(event) => {
+                    setID(event.target.value);
+                }}
+                onKeyDown={(event) => {
+                    if (event.key === 'Enter') {
+                        addToTeam();
+                    }
+                }}
+            />
+            <button onClick={() => addToTeam()}> Print </button>
+            <h1> {data?.name} </h1>
+            {team.map((member, index) => (
+                <h3 onClick={() => removeFromTeam(member)} key={index}>{index + 1 + ". " + member?.name}</h3>
+            ))}
+            <h3> {'>' + note} </h3>
+
+            {printAllImages()}
+
+        </div>
     );
 }
 
